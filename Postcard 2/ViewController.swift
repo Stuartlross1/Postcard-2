ec4d5d998fc4a3e1249c0dd737c763120a03b36d
Stuart Ross <stuartross@stuarts-mac-pro.local>
2017-09-26 20:42:40 +0100

    Initial Commit

---
+++ Postcard 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard 2
+//
+//  Created by Stuart Ross on 26/09/2017.
+//  Copyright © 2017 Stuart Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Postcard 2Tests/Postcard_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Postcard_2Tests.swift
+//  Postcard 2Tests
+//
+//  Created by Stuart Ross on 26/09/2017.
+//  Copyright © 2017 Stuart Ross. All rights reserved.
+//
+
+import XCTest
+@testable import Postcard_2
+
+class Postcard_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
